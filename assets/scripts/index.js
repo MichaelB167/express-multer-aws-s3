55bc97227cafb809d8a335271adaffc3d5171424
MichaelB167 <michael.m.bates@gmail.com>
2016-07-08 00:02:05 -0400

    Complete express aws training

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,17 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+$(() => {
+  $('#application-x-www-form-urlencoded').on('submit', function(e) {
+    let formData = new FormData(this);
+    e.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:3000/uploads',
+      processData: false,
+      data: formData,
+    }).done((data) => console.log(data))
+    .fail((error) => console.error(error));
+  });
+});
